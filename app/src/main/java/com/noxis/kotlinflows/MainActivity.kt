155e5a89be5c9e3f8f38bf0165dcd0b4992177ce
Vycheslav Tumanov <tumanov@umarsh.ru>
2024-03-23 19:14:07 +0300

    Combine, Zip & Merge

--- app/src/main/java/com/noxis/kotlinflows/MainActivity.kt
+++ app/src/main/java/com/noxis/kotlinflows/MainActivity.kt
@@ -3,19 +3,16 @@ package com.noxis.kotlinflows
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -27,46 +24,36 @@ import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
-    private val viewModel: MyViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /**
-         * Этот блок кода реализует поведение как для Jetpack Compose, но при Xml разметки
-         */
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.stateCount.collectLatest { number ->
-//                     binding.textCount.text = number.toString()
-                }
-            }
-        }
-        /**
-         * Можно реализацию вынести в отдельную extens функцию.
-         */
-        collectLatestFlow(viewModel.stateCount) { number ->
-         //   binding.textCount.text = number.toString()
-        }
-
         setContent {
             KotlinFlowsTheme {
                 val viewModel = viewModel<MyViewModel>()
-                val time = viewModel.countDownFlow.collectAsState(initial = MyViewModel.COUNT)
-                val count = viewModel.stateCount.collectAsState(initial = 0)
-                Box(modifier = Modifier.fillMaxSize()) {
-                    Column(
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = viewModel.numberStr)
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly
                     ) {
-                        Text(
-                            text = count.value.toString(),
-                            fontSize = 30.sp,
-                        )
-                        Button(onClick = {
-                            viewModel.incrementCount()
-                        }) {
-                            Text(text = "Inc")
+                        Button(onClick = { viewModel.combineTest() }) {
+                            Text(text = "Combine")
                         }
+                        Button(onClick = { viewModel.auth() }) {
+                            Text(text = "Auth")
+                        }
+                        Button(onClick = { viewModel.clear() }) {
+                            Text(text = "Clear")
+                        }
+                    }
+                    Button(onClick = { viewModel.zipTest() }) {
+                        Text(text = "Zip")
+                    }
+                    Button(onClick = { viewModel.mergeTest() }) {
+                        Text(text = "Merge")
                     }
                 }
             }
--- app/src/main/java/com/noxis/kotlinflows/viewmodel/MyViewModel.kt
+++ app/src/main/java/com/noxis/kotlinflows/viewmodel/MyViewModel.kt
@@ -1,185 +1,67 @@
 package com.noxis.kotlinflows.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.buffer
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.conflate
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.zip
 
-class MyViewModel(
-    private val dispatchers: DispatcherProvider
-) : ViewModel() {
+class MyViewModel : ViewModel() {
 
-    val countDownFlow = flow<Int> {
-        var currentCount = COUNT
-        emit(COUNT)
-        while (currentCount > 0) {
-            delay(1000L)
-            currentCount--
-            emit(currentCount)
-        }
-    }.flowOn(dispatchers.main)
 
-    private val _stateCount = MutableStateFlow(0)
-    val stateCount = _stateCount.asStateFlow()
+    private val isAuthentication = MutableStateFlow(false)
 
-    private val _stateShared = MutableSharedFlow<Int>(replay = 5)
-    val stateShared = _stateShared.asSharedFlow()
+    private val flowZip1 = (1..10).asFlow().onEach { delay(1000L) }
+    private val flowZip2 = (10..20).asFlow().onEach { delay(300L) }
+    private val flowZip3 = (1..5).asFlow().onEach { delay(500L) }
+    var numberStr by mutableStateOf("")
 
-    init {
-      //  collectFlow()
-        squareNumber(3)
-        viewModelScope.launch(dispatchers.main) {
-            stateShared.collect {
-                delay(2000L)
-                println("FIRST FLOW: The receive number: $it")
-            }
-        }
-        viewModelScope.launch(dispatchers.main) {
-            stateShared.collect {
-                delay(3000L)
-                println("SECOND FLOW: The receive number: $it")
-            }
-        }
-        //Только когда есть подписка будет осуществлен выброс emit
-      //  squareNumber(5)
-        //Можно добавить значение replay для sharedFlow
-        //тогда выбросу будут кэшироваться и отдаваться последним подписчикам
+    /**
+     * Возвращает поток, значения которого сгенерированы
+     * с помощью функции transform путем объединения последних выданных значений каждого потока.
+     */
+    fun combineTest() {
+        numberStr = ""
+        isAuthentication.combine(flowZip3) { isAuthentication, num ->
+            numberStr += "isAuthentication: $isAuthentication, Num: $num\n"
+        }.launchIn(viewModelScope)
     }
 
-    fun squareNumber(number: Int) {
-        viewModelScope.launch(dispatchers.main) {
-            _stateShared.emit(number * number)
-        }
+    fun clear() { numberStr = "" }
+    fun auth() {
+        isAuthentication.value = !isAuthentication.value
     }
 
-    fun incrementCount() {
-        _stateCount.value += 1
+    /**
+     * Захватывает значения из текущего потока (this) с другим потоком,
+     * используя предоставленную функцию преобразования,
+     * применяемую к каждой паре значений. Результирующий поток завершается,
+     * как только завершается один из потоков, а на оставшемся потоке вызывается cancel.
+     */
+    fun zipTest() {
+        numberStr = ""
+        flowZip1.zip(flowZip2) { num1, num2 ->
+            numberStr += "[$num1, $num2]\n"
+        }.launchIn(viewModelScope)
     }
 
-
-    private fun collectFlow() {
-        viewModelScope.launch {
-            val flow = flow {
-                delay(250L)
-                emit("Закуска")
-                delay(1000L)
-                emit("Основное блюдо")
-                delay(100L)
-                emit("Десерт")
-            }
-
-            //buffer, conflate, collectLatest
-            println("Collect")
-            flow.onEach {
-                println("Доставлена: $it")
-            }
-                .collect {
-                    println("Сейчас едим: $it")
-                    delay(1500)
-                    println("Закончили с: $it")
-                }
-            /**
-             * Результат, все emit доставлены и обработаны:
-             * Доставлена: Закуска
-             * Сейчас едим: Закуска
-             * Закончили с: Закуска
-             * Доставлена: Основное блюдо
-             * Сейчас едим: Основное блюдо
-             * Закончили с: Основное блюдо
-             * Доставлена: Десерт
-             * Сейчас едим: Десерт
-             * Закончили с: Десерт
-             */
-
-            println("Buffer")
-            flow.onEach {
-                println("Доставлена: $it")
-            }
-                //Оператор буфера создает во время выполнения отдельный
-                // coroutine для потока, к которому он применяется.
-                .buffer()
-                .collect {
-                    println("Сейчас едим: $it")
-                    delay(1500)
-                    println("Закончили с: $it")
-                }
-            /**
-             * Результат с buffer:
-             * Закончили с: Десерт
-             * Доставлена: Закуска
-             * Сейчас едим: Закуска
-             * Доставлена: Основное блюдо
-             * Доставлена: Десерт
-             * Закончили с: Закуска
-             * Сейчас едим: Основное блюдо
-             * Закончили с: Основное блюдо
-             * Сейчас едим: Десерт
-             * Закончили с: Десерт
-             */
-
-            println("Conflate")
-            flow.onEach {
-                println("Доставлена: $it")
-            }
-                //Конфликтует выбросы потока по скомбинированному каналу и
-                // запускает коллектор в отдельном корутине. В результате эмиттер никогда
-                // не приостанавливается из-за медленной работы коллектора,
-                // но коллектор всегда получает самое последнее выданное значение.
-                .conflate()
-                .collect {
-                    println("Сейчас едим: $it")
-                    delay(1500)
-                    println("Закончили с: $it")
-                }
-            /**
-             * Результат с conflate:
-             * Доставлена: Закуска
-             * Сейчас едим: Закуска
-             * Доставлена: Основное блюдо
-             * Доставлена: Десерт
-             * Закончили с: Закуска
-             * Сейчас едим: Десерт
-             * Закончили с: Десерт
-             */
-
-            println("collectLatest")
-            flow.onEach {
-                println("Доставлена: $it")
-            }
-                //Терминальный оператор потока, который собирает заданный поток с
-                // заданным действием. Принципиальное отличие от collect заключается в том,
-                // что когда исходный поток выдает новое значение, блок действий
-                // для предыдущего значения отменяется.
-                .collectLatest {
-                    println("Сейчас едим: $it")
-                    delay(1500)
-                    println("Закончили с: $it")
-                }
-            /**
-             * Результат с collectLatest:
-             * Доставлена: Закуска
-             * Сейчас едим: Закуска
-             * Доставлена: Основное блюдо
-             * Сейчас едим: Основное блюдо
-             * Доставлена: Десерт
-             * Сейчас едим: Десерт
-             * Закончили с: Десерт
-             */
-
-        }
-    }
-
-    companion object {
-        const val COUNT: Int = 10
+    /**
+     * Объединяет заданные потоки в один поток без сохранения порядка элементов.
+     * Все потоки объединяются одновременно, без ограничения
+     * на количество одновременно собираемых потоков.
+     */
+    fun mergeTest() {
+        numberStr = ""
+        merge(flowZip1, flowZip2).onEach {
+            numberStr += "[$it]\n"
+        }.launchIn(viewModelScope)
     }
 }
